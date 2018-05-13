d0a7d57e3310356d0930d5e083077b5bb2152468
asokawotulo <asokawotulo@yahoo.com>
2018-05-13 16:24:19 +0700

    about

--- Front-end/HTML CSS/about.php
+++ Front-end/HTML CSS/about.php
@@ -12,14 +12,14 @@
 <body>
 	<?php include("header.php");?>
 	<div class="container">
-		<h1 class="title">what is <h1 style="color: #CDA74B">iscc</h1></h1>
-		<p class="description"><p style="color: #CDA74B">The annual Information System Case Competition</p><b> organized by BASIS of Binus International is back!!</b></p>
+		<h1 class="title">what is <span style="color: #CDA74B">iscc</span></h1>
+		<p class="content"><span style="color: #CDA74B">The annual Information System Case Competition</span><b> organized by BASIS of Binus International is back!!</b></p>
 		<br>
-		<p class="description">Since its inception in 2014, ISCC has developed a reputation to become one of the most highly coveted case competitions in Indonesia, attracting the attention of top ranked universities from Indonesia and Malaysia.</p>
+		<p class="content">Since its inception in 2014, ISCC has developed a reputation to become one of the most highly coveted case competitions in Indonesia, attracting the attention of top ranked universities from Indonesia and Malaysia.</p>
 		<br>
-		<p class="description">Renowed for its ability to provide quality information systems related cases, ISCC as a competition challenges its participants to think critically in order to produce and pitch an innovative yet applicable solution to relevant problems in modern day socity, with judging panels consisting of local and foreign industry experts and professionals. Designed by world class institutions and multinational corporations, ISCCs' cases has been highly praised by educators of prominent universities, who utilized the cases in their teaching materials.</p>
+		<p class="content">Renowed for its ability to provide quality information systems related cases, ISCC as a competition challenges its participants to think critically in order to produce and pitch an innovative yet applicable solution to relevant problems in modern day socity, with judging panels consisting of local and foreign industry experts and professionals. Designed by world class institutions and multinational corporations, ISCCs' cases has been highly praised by educators of prominent universities, who utilized the cases in their teaching materials.</p>
 		<br>
-		<p class="description">This year, ISCC is back with more challenging cases and more exciting competition structure.</p>
+		<p class="content">This year, ISCC is back with more challenging cases and more exciting competition structure.</p>
 
 	</div>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/trianglify/1.1.0/trianglify.min.js"></script>
